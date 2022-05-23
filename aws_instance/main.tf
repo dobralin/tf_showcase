4d146824e4e5da2a581015c5b5a26cf2653d4673
Alex O <iteratorx@lansator.ro>
2022-05-24 02:00:42 +0300

    add outputs to aws_instance

--- aws_instance/main.tf
+++ aws_instance/main.tf
@@ -29,3 +29,14 @@ resource "aws_instance" "tf_envs_demo_instance_1" {
   	"created_by" = "Terraform TEST"
   }
 }
+
+
+output "ec2_instance_public_ip" {
+	description = "public IP address of instance"
+	value = aws_instance.tf_envs_demo_instance_1.public_ip
+}
+
+output "ec2_instance_public_dns" {
+	description = "public DNS name of instance"
+	value = aws_instance.tf_envs_demo_instance_1.public_dns
+}
